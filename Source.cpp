be07e4c9ae3bf9e50e93b2c833efc0f53b2f2839
09fishboy <vvbbghgghh@gmail.com>
2026-02-10 11:30:56 -0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    cout << "input file name:\n";
+    string A; getline(cin, A);
+    ofstream outputFile(A + ".txt");
+
+    if (outputFile.is_open()) {
+        cout << "Enter text (type '>>>' to finish):\n";
+        outputFile << "<<< Made with FFile >>>\n";
+        bool B = false;
+        while (B == false) {
+            getline(cin, A);
+            if (!(A == ">>>")) {
+                outputFile << A << "\n";
+            }
+            else {
+                B = true;
+            }
+        }
+    }
+    else {
+        cout << "Error: Unable to open the file.\n";
+    }
+    outputFile.close();
+    cout << "file editing done :)\n";
+    cout << "Text has been written to the file successfully.\n";
+
+    return 0;
+}
