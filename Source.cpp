41415b42e0a69da93597e245a6ee8e84d2e1938b
09fishboy <vvbbghgghh@gmail.com>
2026-02-12 13:43:10 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,34 +1,84 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <string>
+#include <ios>
 using namespace std;
 
-int main() {
-    
-    cout << "input file name:\n";
-    string A; getline(cin, A);
-    ofstream outputFile(A + ".txt");
+void G(string F);
 
-    if (outputFile.is_open()) {
-        cout << "Enter text (type '>>>' to finish):\n";
-        outputFile << "<<< Made with FFile >>>\n";
-        bool B = false;
-        while (B == false) {
-            getline(cin, A);
-            if (!(A == ">>>")) {
-                outputFile << A << "\n";
-            }
-            else {
-                B = true;
-            }
+int main() {
+    bool exit = false;
+    while (!exit) {
+        cout << "Do you want to edit or make a txt file? (edit/make/exit): ";
+        string F; getline(cin, F);
+
+        if (F == "edit") {
+            G(F);
+        }
+        else if (F == "make") {
+            G(F);
+        }
+        else if (F == "exit") {
+            exit = true;
+            cout << "Ok bye :)";
+        }
+        else {
+            cout << "Thats not a option man...." << endl;
         }
     }
-    else {
-        cout << "Error: Unable to open the file.\n";
-    }
-    outputFile.close();
-    cout << "file editing done :)\n";
-    cout << "Text has been written to the file successfully.\n";
+}
 
-    return 0;
+void G(string F) {
+    if (F == "make") {
+        cout << "input file name:\n";
+        string A; getline(cin, A);
+        ofstream outputFile(A + ".txt");
+
+        if (outputFile.is_open()) {
+            cout << "Enter text (type '>>>' to finish):\n";
+            outputFile << "<<< Made with FFile >>>\n";
+            bool B = false;
+            while (B == false) {
+                getline(cin, A);
+                if (!(A == ">>>")) {
+                    outputFile << A << "\n";
+                }
+                else {
+                    B = true;
+                    outputFile.close();
+                    cout << "file editing done :)\n";
+                    cout << "Text has been written to the file successfully.\n";
+                }
+            }
+        }
+        else {
+            cout << "Error: Unable to open the file.\n\n";
+        }
+    }
+    else if (F == "edit") {
+        cout << "Say the full file path:\n";
+        string path; getline(cin, path);
+        ofstream outputFile(path, ios::app);
+        if (outputFile.is_open()) {
+            cout << "Enter text (type '>>>' to finish):\n";
+            outputFile << "<<< Made with FFile >>>\n";
+            bool B = false;
+            while (B == false) {
+                string A; getline(cin, A);
+                if (!(A == ">>>")) {
+                    outputFile << A << "\n";
+                }
+                else {
+                    B = true;
+                    outputFile.close();
+                    cout << "file editing done :)\n";
+                    cout << "Text has been written to the file successfully.\n";
+                }
+            }
+        }
+        else {
+            cout << "Error: Unable to open the file :(\n\n";
+        }
+    }
 }
